12e9ea95421a60d99c37b13d7d818fae9bc80415
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2020-03-31 08:15:47 +0900

    Initial repo

---
+++ scripts/auth.js
@@ -0,0 +1,58 @@
+// Tracking Auth Status (listen for auth status changes)
+auth.onAuthStateChanged(user => {
+  if (user) {
+    // get data
+    db.collection('guides')
+      .get()
+      .then(snapShot => {
+        setupGuides(snapShot.docs);
+        setupUI(user);
+      });
+  } else {
+    setupUI();
+    setupGuides([]);
+  }
+});
+
+// Signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  });
+});
+
+// Logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', e => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  });
+});
+
+// Login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // sign up the user
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
